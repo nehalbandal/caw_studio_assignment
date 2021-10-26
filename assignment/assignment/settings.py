d076a100ab89c431b12bff9dea23cbcc3291e2aa
Nehal Bandal <nihalbandal9@gmail.com>
2021-10-26 22:47:49 +0530

    added static root

--- assignment/assignment/settings.py
+++ assignment/assignment/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,6 +130,7 @@ DATETIME_FORMAT = "d M Y H:i:s"
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
